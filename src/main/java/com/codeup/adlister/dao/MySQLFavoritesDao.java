5522b2298c4c066b9a1bda3c2fc39c5e62065df6
Christopher Lundblade <chris.lundblade@gmail.com>
2021-10-29 16:07:57 -0500

    started work on incorporating favorites funcionality including creation of Favorite class and FavoritesDao and some of the methods

---
+++ src/main/java/com/codeup/adlister/dao/MySQLFavoritesDao.java
@@ -0,0 +1,68 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.Favorite;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLFavoritesDao implements Favorites{
+    private Connection connection = null;
+    public MySQLFavoritesDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public List<Favorite> all() {
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM favorites");
+            ResultSet rs = stmt.executeQuery();
+            return createFavoritesFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
+    }
+    private List<Favorite> createFavoritesFromResults(ResultSet rs) throws SQLException {
+        List<Favorite> favorites = new ArrayList<>();
+        while (rs.next()) {
+            favorites.add(extractFavorite(rs));
+        }
+        return favorites;
+    }
+    private Favorite extractFavorite(ResultSet rs) throws SQLException {
+        return new Favorite(
+                rs.getLong("id"),
+                rs.getLong("user_id")
+        );
+    }
+
+    @Override
+    public Long insert(Favorite favorite) {
+        try {
+            String insertQuery = "INSERT INTO favorites(user_id, ad_id) VALUES (?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            stmt.setLong(1, favorite.getUserId());
+            stmt.setLong(2, favorite.getAdId());
+
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new ad.", e);
+        }
+    }
+}
