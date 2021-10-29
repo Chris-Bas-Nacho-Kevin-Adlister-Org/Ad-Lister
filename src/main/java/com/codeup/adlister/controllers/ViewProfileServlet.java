4648e16c098bcb28625016df82087b80e686c8eb
Christopher Lundblade <chris.lundblade@gmail.com>
2021-10-28 21:50:39 -0500

    got first draft of profile page displaying user ads functionality

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,11 +1,18 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.beans.beancontext.BeanContextChild;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
@@ -14,6 +21,17 @@ public class ViewProfileServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        List<Ad> allAds = DaoFactory.getAdsDao().all();
+        List<Ad> userAds = new ArrayList<Ad>();
+        User currentUser = ((User) request.getSession().getAttribute("user"));
+        for(Ad ad : allAds){
+            if(ad.getUserId() == currentUser.getId()){
+                userAds.add(ad);
+                System.out.println("Found an ad by this user");
+            }
+        }
+        request.setAttribute("userAds", userAds);
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -1,9 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
         <jsp:param name="title" value="Your Profile" />
     </jsp:include>
+
 </head>
 <body>
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
@@ -11,6 +13,18 @@
     <div class="container">
         <h1>Welcome, ${sessionScope.user.username}!</h1>
     </div>
+
+    <c:forEach var="ad" items="${userAds}">
+        <div class="col-md-6">
+            <h2>${ad.title}</h2>
+            <p>${ad.description}</p>
+            <p>${ad.condition}</p>
+            <p>${ad.postDate}</p>
+            <p>${ad.priceInCents}</p>
+            <p>${ad.location}</p>
+            <p>${ad.category}</p>
+        </div>
+    </c:forEach>
 
 </body>
 </html>
